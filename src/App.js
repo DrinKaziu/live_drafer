ad2fb4395b5b273fe03077f0177f273630405ec3
SnowSurfer <drinkaziu@gmail.com>
2019-05-11 01:32:33 -0400

    create a seed file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import players from './seedPlayers';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {players.map(player => <p>{player.fname} {player.lname}</p>)}
     </div>
   );
 }
